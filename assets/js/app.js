d1c3c1250970a59e83d354343d7c74f30cd2118d
ankareta <carolinankaret@gmail.com>
2023-10-03 16:24:14 -0600

    Se corrigieron los errores y se agregaron comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//faltaba un punto por ser clase
+const $b = document.querySelector('.blog');//se cambio el "#" por "." porque es una clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Se tiene que poner async antes de function porque solo se puede usar await si es una funcion asincrona
+async function displayUser(username) {
+  
   $n.textContent = 'cargando...';
+  // try {
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  const data = await response.json();  // Agregamos  "await response.json()" para esperar la respuesta del servidor y guardar la informacion en la constante "data"
+  console.log(data); 
+
+  $n.textContent = data.name;
+  $b.textContent = data.blog; 
+  $l.textContent = data.location;
+} //corregimos la sintaxis
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // se agregó el "$" antes de la n, como esta definida antes.
 }
 
 displayUser('stolinski').catch(handleError);
+/* Este codigo llama a una API para obtener los datos: name, blog y location, en caso de error llama a la funcion handleError y mostrar en el DOM que algo salio mal*/
